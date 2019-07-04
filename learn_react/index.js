004747d6287b28bfe008f66a916442fdc13ee2d8
LeoKnox <LeoKnox@users.noreply.github.com>
2019-07-04 16:28:15 -0700

    Update index.js

--- learn_react/index.js
+++ learn_react/index.js
@@ -1,1 +1,17 @@
-ReactDOM.render(<h1>Hello, World</h1>, document.getElementById("root");
+import React from "react"
+import ReactDOM from "react-dom"
+
+function MyApp() {
+  return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  )
+}
+
+ReactDOM.render(
+  <MyApp />,
+  document.getElementById("root")
+)
