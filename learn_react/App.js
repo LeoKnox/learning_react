ca4724f031e3bd287737c14dfdfb8bc4c203c114
LeoKnox <LeoKnox@users.noreply.github.com>
2019-07-05 07:10:22 -0700

    Create App.js

---
+++ learn_react/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function App() {
+    return(
+        <div>
+    <form>
+        <input type="checkbox"/><span>work hard</span><br/>
+        <input type="checkbox"/><span>work harder</span><br/>
+        <input type="checkbox"/><span>do pull-up</span><br/>
+    </form>
+    </div>
+    )
+}
+
+export default App
